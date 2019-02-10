a63e65b551460acf8a727248dab0a5eb799078c5
Ben <bengundy@gmail.com>
2019-02-09 21:17:40 -0500

    2-9-2019, homework in progress
    Most of the way through basic methods.

---
+++ MySQLConnectTest/src/Tester.java
@@ -0,0 +1,18 @@
+
+public class Tester {
+
+	public static void main(String[] args) {
+		/*Genre g = new Genre(5);
+		g.setGenreName("Americana");
+		g.setDescription("Music your dad likes");
+		*/
+		
+		//Genre g1 = new Genre("Folk", "Music your grandpa likes");
+		
+		Genre g2 = new Genre(5);
+		System.out.println(g2.getGenreName());
+		System.out.println(g2.getDescription());
+	}
+
+}
+ 
